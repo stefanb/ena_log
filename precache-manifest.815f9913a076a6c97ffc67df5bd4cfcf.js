657ab1685aa52d931e8921c0b80edbe795616d36
Felix Lenders <fl@felixlenders.de>
2020-08-22 20:54:01 +0200

    Updates

---
+++ precache-manifest.815f9913a076a6c97ffc67df5bd4cfcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1020e756e7fb58db72684914dcff44",
+    "url": "/ena_log/index.html"
+  },
+  {
+    "revision": "7caa0aaca13bb5407df7",
+    "url": "/ena_log/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a50ebe69088775c81708",
+    "url": "/ena_log/static/css/main.1a81e7c5.chunk.css"
+  },
+  {
+    "revision": "7caa0aaca13bb5407df7",
+    "url": "/ena_log/static/js/2.7bf9be31.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/ena_log/static/js/2.7bf9be31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a50ebe69088775c81708",
+    "url": "/ena_log/static/js/main.1ec46286.chunk.js"
+  },
+  {
+    "revision": "e09c2f3481808af57442",
+    "url": "/ena_log/static/js/runtime-main.e7d88cf3.js"
+  }
+]);
